d6f9024a7e4e4780f6e1a6a2787ce8079a483694
Jp Fortuno <jp.fortuno@nullpod.com>
2023-05-30 08:28:13 +0400

    added even more policies

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,7 +3,7 @@ class RestaurantsController < ApplicationController
 
   # GET /restaurants or /restaurants.json
   def index
-    @restaurants = Restaurant.all
+    @restaurants = policy_scope(Restaurant)
   end
 
   # GET /restaurants/1 or /restaurants/1.json
