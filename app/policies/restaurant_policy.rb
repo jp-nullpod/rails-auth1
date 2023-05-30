a833b4ca0796ccd0bdeed6e3a0a7da0b7f0cd3bb
Jp Fortuno <jp.fortuno@nullpod.com>
2023-05-30 10:01:40 +0400

    policy mods

--- app/policies/restaurant_policy.rb
+++ app/policies/restaurant_policy.rb
@@ -2,7 +2,6 @@ class RestaurantPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     def resolve
-      # scope.all
       user.admin? ? scope.all : scope.where(user: user)
     end
   end
